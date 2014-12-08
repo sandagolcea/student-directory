7fcc8c0caad03be9d880fd3767ce000b602cb16f
Sanda Golcea <sanda.golcea@gmail.com>
2014-12-08 15:49:54 +0000

    Adds printing students by cohort

--- directory.rb
+++ directory.rb
@@ -35,7 +35,7 @@ def input_students
   @ask.call
 
   while !@name.empty? do
-  	students << {:name => @name, :cohort => @cohort}
+  	students << {:name => @name, :cohort => @cohort.capitalize.to_sym}
   	puts "Now we have #{students.length} students"
     @ask.call
   end
@@ -43,7 +43,27 @@ def input_students
   students
 end
 
+# def sort_cohort(students)
+#   students
+     # student.each.cohort.value.sort
+# end
+# ask how this is done with sort ..sort for hash ?!
+# && how to do it with map ? (map() )  pls look over map again.
+
+def print_cohort(students)
+  # make list of cohorts
+  cohorts = []
+  students.each {|student| cohorts << student[:cohort]}
+  cohorts.uniq!
+
+    cohorts.each do |cohort| 
+      puts "Students in the #{cohort} cohort :".center(LINESIZE)
+      students.each { |student| puts student[:name] if student[:cohort] == cohort }
+    end
+end
+
 students = input_students
 print_header
 print(students)
+print_cohort(students)
 print_footer(students)
