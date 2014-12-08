c35472ede3b3ea16d72dcc560d8fffed42126270
Sanda Golcea <sanda.golcea@gmail.com>
2014-12-08 00:00:55 +0000

    Added command line input for students

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 
 # make the list/array of students
-students = [ 
-	{:name => "Dr. Hannibal Lecter", :cohort => :december},
-	{:name => "Darth Vader", :cohort => :december},
-	{:name => "Nurse Ratched", :cohort => :december},
-	{:name => "Michael Corleone", :cohort => :december},
-	{:name => "Alex De Large", :cohort => :december},
-	{:name => "The Alien", :cohort => :december},
-	{:name => "Terminator", :cohort => :december},
-	{:name => "Freddy Kruger", :cohort => :december},
-	{:name => "The Joker", :cohort => :december}
-]
+# students = [ 
+# 	{:name => "Dr. Hannibal Lecter", :cohort => :december},
+# 	{:name => "Darth Vader", :cohort => :december},
+# 	{:name => "Nurse Ratched", :cohort => :december},
+# 	{:name => "Michael Corleone", :cohort => :december},
+# 	{:name => "Alex De Large", :cohort => :december},
+# 	{:name => "The Alien", :cohort => :december},
+# 	{:name => "Terminator", :cohort => :december},
+# 	{:name => "Freddy Kruger", :cohort => :december},
+# 	{:name => "The Joker", :cohort => :december}
+# ]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -27,6 +27,23 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+  	students << {:name => name, :cohort => :december}
+  	puts "Now we have #{students.length} students"
+  	name = gets.chomp
+  end
+
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
